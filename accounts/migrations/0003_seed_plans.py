e92fbc4d9494dd8e8deb6a938f2b27aa95336b58
Amin <szmoamin@gmail.com>
2025-08-09 16:23:42 +0330

    Better ui 1

--- accounts/migrations/0003_seed_plans.py
+++ accounts/migrations/0003_seed_plans.py
@@ -4,11 +4,11 @@ from django.db import migrations
 def seed_plans(apps, schema_editor):
     Plan = apps.get_model('accounts', 'Plan')
     items = [
-        {"name": "پلن ۲۰ گیگابایت", "description": "حجم: ۲۰ گیگ", "price_irr": 6500000, "duration_days": 30, "data_gb": 20, "order_index": 1},
-        {"name": "پلن ۳۰ گیگابایت", "description": "حجم: ۳۰ گیگ", "price_irr": 8500000, "duration_days": 30, "data_gb": 30, "order_index": 2},
-        {"name": "پلن ۵۰ گیگابایت (پیشنهاد ویژه)", "description": "حجم: ۵۰ گیگ", "price_irr": 12000000, "duration_days": 30, "data_gb": 50, "order_index": 3},
-        {"name": "پلن ۷۰ گیگابایت", "description": "حجم: ۷۰ گیگ", "price_irr": 15000000, "duration_days": 30, "data_gb": 70, "order_index": 4},
-        {"name": "پلن ۱۰۰ گیگابایت (به‌صرفه‌ترین)", "description": "حجم: ۱۰۰ گیگ", "price_irr": 19000000, "duration_days": 30, "data_gb": 100, "order_index": 5},
+        {"name": "پلن ۲۰ گیگابایت", "description": "حجم: ۲۰ گیگ", "price_irr": 650000, "duration_days": 30, "data_gb": 20, "order_index": 1},
+        {"name": "پلن ۳۰ گیگابایت", "description": "حجم: ۳۰ گیگ", "price_irr": 850000, "duration_days": 30, "data_gb": 30, "order_index": 2},
+        {"name": "پلن ۵۰ گیگابایت (پیشنهاد ویژه)", "description": "حجم: ۵۰ گیگ", "price_irr": 1200000, "duration_days": 30, "data_gb": 50, "order_index": 3},
+        {"name": "پلن ۷۰ گیگابایت", "description": "حجم: ۷۰ گیگ", "price_irr": 1500000, "duration_days": 30, "data_gb": 70, "order_index": 4},
+        {"name": "پلن ۱۰۰ گیگابایت (به‌صرفه‌ترین)", "description": "حجم: ۱۰۰ گیگ", "price_irr": 1900000, "duration_days": 30, "data_gb": 100, "order_index": 5},
     ]
     for i in items:
         Plan.objects.update_or_create(name=i["name"], defaults=i)
