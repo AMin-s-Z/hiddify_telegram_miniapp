b8edda25bc4cc3be95b4fd49d8bc0f7546eb3c14
Amin <szmoamin@gmail.com>
2025-08-14 23:27:30 +0330

    big change3

--- hiddify_telegram_miniapp/settings.py
+++ hiddify_telegram_miniapp/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / '.env')
+load_dotenv(BASE_DIR / '.env.example')
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
