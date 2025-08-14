a708b0ebc1f2cfcd00b0b9bbcf37a9bcccd01ac3
Amin <szmoamin@gmail.com>
2025-08-14 23:34:04 +0330

    big change3

--- hiddify_telegram_miniapp/settings.py
+++ hiddify_telegram_miniapp/settings.py
@@ -52,11 +52,11 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
+        'NAME':"database",
+        'USER': "admin",
         'PASSWORD': "admin1234",
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'HOST': "localhost",
+        'PORT': "5432",
     }
 }
 
